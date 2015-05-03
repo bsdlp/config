b6f88e6e7bc7bae19600694c0aeaf6d8698608be
Jon Chen <bsd@voltaire.sh>
2015-05-03 00:49:01 -0700

    add Load() function

--- config.go
+++ config.go
@@ -2,9 +2,14 @@ package config
 
 import (
 	"errors"
+	"fmt"
+	"io/ioutil"
 	"os/user"
 	"path/filepath"
+	"reflect"
 	"strings"
+
+	"gopkg.in/yaml.v2"
 )
 
 type ConfigNamespace struct {
@@ -13,10 +18,38 @@ type ConfigNamespace struct {
 }
 
 type Config interface {
-	Load() error
+	Load(src string, dst interface{}) error
 }
 
 const UserBase string = "~/.config/"
+
+// Load expands the provided src path using config.ExpandUser, then reads
+// the file and unmarshals into dst using go-yaml.
+func Load(src string, dst interface{}) (err error) {
+	dstv := reflect.ValueOf(dst)
+
+	if dstv.Kind() != reflect.Ptr {
+		err = errors.New("config: not a pointer.")
+	} else if dstv.IsNil() {
+		err = fmt.Errorf("nil %s.", reflect.TypeOf(dstv).String())
+	}
+	if err != nil {
+		return
+	}
+
+	path, err := ExpandUser(src)
+	if err != nil {
+		return
+	}
+
+	data, err := ioutil.ReadFile(path)
+	if err != nil {
+		return
+	}
+
+	err = yaml.Unmarshal(data, &dst)
+	return
+}
 
 func ExpandUser(path string) (exPath string, err error) {
 	// Acts kind of like os.path.expanduser in Python, except only supports
